3d69430cfaf5aace63d43e012272e0f5f0d7ada1
adam.olser <adam.olser@kiwi.com>
2020-12-29 00:21:00 +0100

    add react-redux

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+  popular: [],
+}
+
+const gamesReducer = (state=initState, action) => {
+  switch (action.type){
+    case "FETCH_GAMES":
+      return {...state}
+    default:
+      return {...state}
+  }
+}
+
+export default gamesReducer
